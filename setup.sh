b4c5c2f7f61f900f8b20beca1e695a73b84567e7
Bodaxxt <161380943+Bodaxxt@users.noreply.github.com>
2025-05-09 04:22:56 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
-!/bin/bash
+#!/bin/bash
 
-# Install required system packages
-apt-get update -y
-apt-get install -y python3-distutils python3-dev gcc g++ build-essential
+# Install system dependencies
+apt-get update -y && apt-get install -y \
+    python3-distutils \
+    python3-dev \
+    gcc \
+    g++ \
+    build-essential
 
 # Upgrade pip
 pip install --upgrade pip
 
-# Install dependencies
+# Install Python packages
 pip install -r requirements.txt
