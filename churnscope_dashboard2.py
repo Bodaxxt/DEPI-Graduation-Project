1bdf571a7bbc074e1faa6cdbe2f564c2e85307f4
Bodaxxt <161380943+Bodaxxt@users.noreply.github.com>
2025-05-09 04:46:30 +0300

    Update churnscope_dashboard2.py

--- churnscope_dashboard2.py
+++ churnscope_dashboard2.py
@@ -27,7 +27,7 @@ from imblearn.over_sampling import RandomOverSampler
 from imblearn.pipeline import Pipeline as ImbPipeline
 from sklearn.metrics import roc_auc_score, classification_report
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 # Set page config
 st.set_page_config(
